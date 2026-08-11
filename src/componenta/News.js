e3938c92b585903a4463d3f877af2fa8d5238622
M-shahzaib-khan <i230850@isb.nu.edu.pk>
2026-08-11 15:56:52 +0500

    initial commit

---
+++ src/componenta/News.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Newsitems from './Newsitems'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        this is a news component
+        <Newsitems/>
+        
+      </div>
+    )
+  }
+}
+
+export default News
---
+++ src/componenta/Newsitems.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Newsitems extends Component {
+  render() {
+    return (
+      <div>
+        this is a news items component
+        
+      </div>
+    )
+  }
+}
+
+export default Newsitems
